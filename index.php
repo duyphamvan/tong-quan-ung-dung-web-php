9653dc986c895d6510b2f91707d4ba268593bacb
duy <phamduychelsea@gmail.com>
2019-01-21 16:02:55 +0700

    a

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Current server time</title>
+</head>
+<body>
+	<h1> Current Sever Time is: <?php echo date('Y-M-d h:m:s') ?></h1>
+</body>
+</html>
